dafa55d43987e318f1dacd31dc183bc7044216e5
HarshadChormare <96374853+Harshadchormare@users.noreply.github.com>
2024-06-20 09:56:32 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,16 +1,22 @@
-function openForm() {
-    document.getElementById("contactForm").style.display = "block";
-}
-
 function closeForm() {
-    document.getElementById("contactForm").style.display = "none";
-    document.getElementById("thank-you-message").style.display = "none";
-    document.getElementById("contact-us-form").style.display = "block";
-    document.getElementById("contact-us-form").reset();
-}
+        document.getElementById('contactForm').style.display = 'none';
+        document.getElementById('thank-you-message').style.display = 'none';
+        document.getElementById('contact-us-form').style.display = 'block';
+        document.getElementById('form-title').style.display = 'block';
+        document.getElementById('contact-us-form').reset();
+    }
 
-document.getElementById("contact-us-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("contact-us-form").style.display = "none";
-    document.getElementById("thank-you-message").style.display = "block";
-});
+    function handleSubmit(event) {
+        event.preventDefault();
+        document.getElementById('contact-us-form').style.display = 'none';
+        document.getElementById('form-title').style.display = 'none';
+        document.getElementById('thank-you-message').style.display = 'block';
+    }
+
+    function openForm() {
+        document.getElementById('contactForm').style.display = 'block';
+        document.getElementById('contact-us-form').style.display = 'block';
+        document.getElementById('form-title').style.display = 'block';
+        document.getElementById('thank-you-message').style.display = 'none';
+    }
+</script>
